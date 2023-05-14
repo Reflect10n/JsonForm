f454db58f91943df7eb2f40b87bbe7d620f074d9
Reflect10n <133597638+Reflect10n@users.noreply.github.com>
2023-05-14 22:14:31 +0400

    Add files via upload

---
+++ JsonForm/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using JsonForm.Models;
+
+namespace JsonForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public HomeController(ILogger<HomeController> logger,
+            IWebHostEnvironment webHostEnvironment)
+        {
+            _logger = logger;
+            this.webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(PersonData personData)
+        {
+            if (ModelState.IsValid)
+            {
+                var jSerializerSettings = new JsonSerializerSettings { DateFormatString = "yyyy/MM/dd" };
+                var json = JsonConvert.SerializeObject(personData, jSerializerSettings);
+                JObject obj = JObject.Parse(json);
+                CreateFile(obj);
+                return View("Successful");
+            }
+            else
+                return View("Index");
+        }
+
+        private void CreateFile(JObject obj)
+        {
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory + "Created Jsons\\";
+            try
+            {
+                Directory.CreateDirectory(currentDirectory);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Не удалось создать папку в указанной директории", exception);
+                return;
+            }
+            var fileName = DateTime.Now.ToString("ddMMyy_HHmmss_ffff") + ".json";
+            string filePath = Path.Combine(currentDirectory, fileName);
+            System.IO.File.WriteAllText(filePath, obj.ToString());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ JsonForm/Models/PersonData.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Windows.Markup;
+
+namespace JsonForm.Models
+{
+    public class PersonData
+    {
+        [Required(ErrorMessage = "Поле обязательно к заполнению!")]
+        [RegularExpression(@"^[A-Za-z]+([\'\-\ A-Za-z][A-Za-z\'\-]*)*|^[А-ЯЁа-яё]+([\'\-\ А-ЯЁа-яё][А-ЯЁа-яё\'\-]*)*", 
+            ErrorMessage = "Можно вводить только русские ИЛИ латинские буквы! А также некоторые знаки (пробел, апостроф, тире)")]
+        [Display(Name = "Введите фамилию")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Поле обязательно к заполнению!")]
+        [Display(Name = "Введите имя")]
+        [RegularExpression(@"^[A-Z]+([\'\-\ A-Za-z][A-Za-z\'\-]*)*|^[А-ЯЁ]+([\'\-\ А-ЯЁа-яё][А-ЯЁа-яё\'\-]*)*",
+            ErrorMessage = "Можно вводить только русские ИЛИ латинские буквы! А также некоторые знаки (пробел, апостроф, тире). Имя должно начинаться с заглавной буквы!")]
+        public string FirstName { get; set; }
+
+
+        [Display(Name = "Введите отчество")]
+        [RegularExpression(@"^[A-Za-z]+([\'\-\ A-Za-z][A-Za-z\'\-]*)*|^[А-ЯЁа-яё]+([\'\-\ А-ЯЁа-яё][А-ЯЁа-яё\'\-]*)*",
+            ErrorMessage = "Можно вводить только русские ИЛИ латинские буквы! А также некоторые знаки (пробел, апостроф, тире)")]
+        public string? MiddleName { get; set; }
+
+        [Required(ErrorMessage = "Поле обязательно к заполнению")]
+        [Display(Name = "Введите дату рождения")]
+        [DataType(DataType.Date)]
+        public DateTime? BirthDate { get; set; }
+    }
+}
